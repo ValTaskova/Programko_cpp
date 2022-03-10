751d3c7306e59ac3b4c16e3592ef33fb09c040db
valik <val.taskova@gmail.com>
2022-03-10 16:05:28 +0100

    stvrta hodina

--- main.cpp
+++ main.cpp
@@ -5,90 +5,48 @@ const int x = 6;    //globalna premenna, je vidiet v celom programe
 
 int main() {        //vo funkcii su lokalne premenne, vidiet je len vo funkcii main
 
-    //inicializovane premenne
-    int cislo = 1;
-    double cislo2 = 1.5;
-    char znak = 'c';
-    bool hodnota = true;
+    int c;
 
-    //deklarovane premenne
-    int premenna;
-    double premenna2;
-    char c;
+    std::cin >> c;// 'a' 'z' 
 
-    premenna = 8;
+  // < > == <= >= !=
+  // && -a, || - alebo
+    
+  /* if (c < '0' || c > '9') { //cislo od '0' po '9'
+       std::cout << "je to cislo" <<std::endl;
+   } 
+   else if (c >= 'a' && c <= 'z') { //male pismeno od 'a' po 'z' 
+       std::cout << "je to male pismeno" <<std::endl;
+   }
+   else if (c >= 'A' && c <= 'Z') { //velke pismeno od 'A' po 'Z'
+        std::cout << "je to velke pismeno" <<std::endl;
+   }
+    
+   else {
+       std::cout << "je to nieco ine" <<std::endl;
+   }*/
 
-
-    int e, d, w, w2; 
-
-    e = 5 + znak;       //v premennej znak je ulozena hodnota 'c', ktora ma ascii hodnotu 99
-    std::cout << e << std::endl; 
-
-    unsigned char z = 129;      //vypise hodnotu 129, pokial odstranime unsigned, tak vypise hodnotu -127 
-    std::cout << int(z) << std::endl; 
-
-    cislo = 3;
-    e = 9;
-    d = 5;
-
-    //Inkremntacie: premenna++, ++premenna
-    //Dekremntacia: premenna--, --premenna
-    w = e + d++ - cislo--; // 8 + 2++ - 3-- = 7 => d =3, cislo = 2
-    w2 = 11 - ++d * e; // 11 - 4 * 8 = -21
-
-    std::cout << w << std::endl;
-    std::cout << w2 << std::endl;
-
-
-    std::cin >> z;      //nacita zadanu klavesu do premennej z
-    if(e > d) {         //porovnava ci hodnota v premennej e je vacsia nez v premennej d
-        int t = 6;      //lokalna premenna, ktora je vidiet len v tejto podmienke if
-        std::cout << "Zadali ste: " << z << " a jeho je " << int(z) << std::endl; //vypise sa, ak je podmienka e>d pravdiva
-    }
-    else {
-        std::cout << "Nebola to pravda" << std::endl; //vypise sa, ak je podmienka e>d nepravdiva
+    switch (c)
+    {
+    case 1:
+        std::cout << "cislo 1" << std::endl;
+        //break;
+    case 2:
+        std::cout << "cislo 2" << std::endl;
+        break;
+    case 3:
+        std::cout << "cislo 3" << std::endl;
+        break;
+    case 4:
+        std::cout << "cislo 4" << std::endl;
+        break;
+    
+    default: std::cout << "default" << std::endl;
+        break;
     }
 
     
-    double k = 6.8, l;
-    e = 9;
-    d = 5;
-
-    l = round(k); //round() => funkcia v cmath, ktora zaokruhluje
-    w = e % d; // 9/5 
-     
-    std::cout << l << std::endl;
-    std::cout << round(k) << std::endl;
-    std::cout << w << std::endl;
-
-
-    std::cout << 4 + 5 << std::endl;
-    std::cout << 4 - 5 << std::endl;
-    std::cout << 4 * 5 << std::endl;
-    std::cout << 4 % 5 << std::endl;
-    std::cout << int(c) << std::endl;
-
-    cislo = 10;
-    std::cout << cislo << std::endl;
-
-    cislo += 5; //k premennej cislo sa pripocita hodnota 5 a ulozi sa do premennej cislo
-    std::cout << cislo << std::endl;
-
-    cislo -= 7;
-    std::cout << cislo << std::endl;
-
-    cislo *= 2;
-    std::cout << cislo << std::endl;
-
-    cislo /= 2;
-    std::cout << cislo << std::endl;
-
-    cislo %= 6;
-    std::cout << cislo << std::endl;
-     
     
-    std::cout << "Toto je retazec so specialnymi znakmi: \"Valika\", \\" << std::endl;
-
    
     return 0;
 }
