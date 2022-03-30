12ccd0a63e950957321d010e3d0746aa1d3968c1
valik <val.taskova@gmail.com>
2022-03-30 20:04:04 +0200

    siedma hodina

--- main.cpp
+++ main.cpp
@@ -2,41 +2,81 @@
 #include <string> 
 
 void opakovanie(int pocet);
+int sucet(int a, int b);
+
 
 int main() {        
    
-    int num = 15;
-    opakovanie(num);
+    int num ;
+
+    num = sucet(5, 6);
+    opakovanie(5);
+
+
+    int pole[5] = {77, 99, 55, 66, 33}, nacitavacie_pole[5];
+
+        //{77, 99, 55, 66, 33}
+        // 0  1  2  3   4
+    for (int i = 0; i < 5; i++)         //vypise pole
+        std::cout << pole[i] << std::endl;
+    std::cout << pole[3];       //vypise cislo 66 teda treti prvok v poli
+
+
+    for (int i = 0; i < 5; i++)         //nacitavanie do nacitavacie_pole
+        std::cin >> nacitavacie_pole[i];
+
+    for (int i = 0; i < 5; i++)         //vzpise nacitavacie_pole
+        std::cout << nacitavacie_pole[i] << std::endl;
+
+       
     
-    std::cout << std::endl << "Vnoreny For cyklus" << std::endl;
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
-           if (i == j) 
-                std::cout << '*';
-            else if (i == (10-j-1))
-                std::cout << '*';
-           else                     //skuste vynechat else a porozmyslajte co sa stalo a preco sa to stalo
-                std::cout << '-';
-        }
-        std::cout << std::endl;
-    }
-   
-    std::cout << std::endl << "While cyklus" << std::endl;
-    int j = 0;      //skuste inicializovat k na 10
-    while (j < 10) {
-        std::cout << "j je: " << j << std::endl;
-        j++;
-        
+    int cislo;
+
+    std::cin >> cislo;
+
+    if (cislo <= 0) {       //overujeme ci je cislo kladne, ak nie je program skonci s chybou
+        return 1;
     }
 
-    std::cout << std::endl << "Do-while cyklus" << std::endl;
-    int k = 0;      //skuste inicializovat k na 10
-    do
-    {
-        std::cout << "k je: " << k << std::endl;
-        k++;
-    } while (k < 10);       //nie je rozdiel medzi while a tymto do-while cyklom
+    std::cout << std::endl << "Vnoreny For cyklus" << std::endl;
+    for (int i = 0; i < cislo; i++) {         //cislo = 3 a ma vystup: 00 01 02 10 11 12 20 21 22
+        for (int j = 0; j < cislo; j++) {     //ide o dve premenne VEDLA SEBA, nie je to jedno cislo  
+
+            std::cout << i << j << std::endl;
+           
+        }
+    } 
     
+    
+    std::cout << std::endl << "While cyklus" << std::endl;
+    int j = 0;      //skuste inicializovat k na 10
+
+    while (j < 10) {
+         
+        j++;
+        //getline (std::cin, text);
+
+        if (j == 5) {       //pri cisle 5, cyklus yastavi a hned prejde na dalsiu iteraciu == nevypise sa 5
+            continue;
+        }
+         std::cout << j << std::endl;  
+    } 
+
+    std::string text;
+    getline (std::cin, text);
+
+    while (text == "siedma kapitola") {     //dokial nenapiseme text "siedma kapitola" tak bude cyklus stale bezat a nacitavat text
+         
+        getline (std::cin, text); 
+
+        if (text == "odid")    //ak napiseme "odid" tak prikaz break nas vyhodi z cyklu
+            break; 
+        
+    } 
+
+    std::cout << "Dakujem";
+
+     
    
     return 0;
 }
@@ -49,3 +89,7 @@ void opakovanie(int pocet){
             std::cout << i << std::endl;
         }
 } 
+
+int sucet(int a, int b) {
+    return a+b;
+}
