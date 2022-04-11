23438cf5beb97361e0387b794b0c7b745a7e9d0a
valik <val.taskova@gmail.com>
2022-04-11 13:50:10 +0200

    Osma hodina

--- main.cpp
+++ main.cpp
@@ -1,95 +1,127 @@
 #include <iostream>
-#include <string> 
+#include <string>
+#include <cstdlib>
+#include <ctime> 
+#include <climits>
 
-void opakovanie(int pocet);
-int sucet(int a, int b);
-
+double vypocitaj_priemer(int pole[]);
+void vypocitaj_max(int pole[]);
+void vypocitaj_min(int pole[]);
+void vypocitaj_vacsie_nez_priemer(int pole[], double priemer);
+void vypocitaj_mensie_nez_priemer(int pole[], double priemer);
 
 int main() {        
-   
-    int num ;
-
-    num = sucet(5, 6);
-    opakovanie(5);
-
-
-    int pole[5] = {77, 99, 55, 66, 33}, nacitavacie_pole[5];
-
-        //{77, 99, 55, 66, 33}
-        // 0  1  2  3   4
-    for (int i = 0; i < 5; i++)         //vypise pole
+    
+    double priemer;
+    int pole[100];
+    srand((unsigned) time(0));
+    
+    for (int i = 0; i < 100; i++)
+    {
+        pole[i] = rand();
         std::cout << pole[i] << std::endl;
-    std::cout << pole[3];       //vypise cislo 66 teda treti prvok v poli
-
-
-    for (int i = 0; i < 5; i++)         //nacitavanie do nacitavacie_pole
-        std::cin >> nacitavacie_pole[i];
-
-    for (int i = 0; i < 5; i++)         //vzpise nacitavacie_pole
-        std::cout << nacitavacie_pole[i] << std::endl;
-
-       
-    
-    int cislo;
-
-    std::cin >> cislo;
-
-    if (cislo <= 0) {       //overujeme ci je cislo kladne, ak nie je program skonci s chybou
-        return 1;
     }
-
-    std::cout << std::endl << "Vnoreny For cyklus" << std::endl;
-    for (int i = 0; i < cislo; i++) {         //cislo = 3 a ma vystup: 00 01 02 10 11 12 20 21 22
-        for (int j = 0; j < cislo; j++) {     //ide o dve premenne VEDLA SEBA, nie je to jedno cislo  
-
-            std::cout << i << j << std::endl;
-           
-        }
-    } 
     
-    
-    std::cout << std::endl << "While cyklus" << std::endl;
-    int j = 0;      //skuste inicializovat k na 10
+    int menu = 0;
 
-    while (j < 10) {
-         
-        j++;
-        //getline (std::cin, text);
+    while (menu != 6) {
 
-        if (j == 5) {       //pri cisle 5, cyklus yastavi a hned prejde na dalsiu iteraciu == nevypise sa 5
-            continue;
-        }
-         std::cout << j << std::endl;  
-    } 
-
-    std::string text;
-    getline (std::cin, text);
-
-    while (text == "siedma kapitola") {     //dokial nenapiseme text "siedma kapitola" tak bude cyklus stale bezat a nacitavat text
-         
-        getline (std::cin, text); 
-
-        if (text == "odid")    //ak napiseme "odid" tak prikaz break nas vyhodi z cyklu
-            break; 
+        switch (menu)
+        {
+        case 1:
+            priemer = vypocitaj_priemer(pole);
+            break;
+        case 2:
+            vypocitaj_max(pole);
+            break;
+        case 3:
+            vypocitaj_min(pole);
+            break;
+        case 4:
+            vypocitaj_vacsie_nez_priemer(pole, priemer);
+            break;
+        case 5:
+            vypocitaj_mensie_nez_priemer(pole, priemer);
+            break;
         
-    } 
+        default:
+            break;
+        }
+    
+    
+    
+    std::cout << "Najvacsie cislo: " << max << std::endl;
+    
+    int min = INT_MAX;
 
-    std::cout << "Dakujem";
+    for (int i = 0; i < 100; i++)
+    {
+        if (pole[i] < min) {
+            min = pole[i];
+        }
+    }
+    
+    std::cout << "Najmensie cislo: " << min << std::endl;
 
-     
+    int pocet = 0;
+
+    for (int i = 0; i < 100; i++)
+    {
+        if (pole[i] > priemer) {
+            pocet++;
+        }
+    }
+    
+    std::cout << "Pocet vacsich cisle nez priemer: " << pocet << std::endl;
+
+    int pocet1 = 0;
+
+    for (int i = 0; i < 100; i++)
+    {
+        if (pole[i] < priemer) {
+            pocet1++;
+        }
+    }
+    
+    std::cout << "Pocet mensich cisle nez priemer: " << pocet1 << std::endl;
+    }
    
     return 0;
 }
 
-void opakovanie(int pocet){
+double vypocitaj_priemer(int pole[]) {
+    int sum = 0;
+    double priemer;
 
-    std::cout << "For cyklus" << std::endl << "Pocet iteracii je " << pocet << std::endl;
+    for (int i = 0; i < 100; i++)
+    {
+        sum += pole[i];
+    }
+    
+    priemer = (double)sum / 100;
 
-    for (int i = 0; i < pocet; i++) {
-            std::cout << i << std::endl;
+    std::cout << "Sucet: " << sum << std::endl;
+    std::cout << "Priemer: " << priemer << std::endl;
+
+    return priemer;
+}
+
+void vypocitaj_max(int pole[]) {
+int max = INT_MIN;
+
+    for (int i = 0; i < 100; i++)
+    {
+        if (pole[i] > max) {
+            max = pole[i];
         }
-} 
+    }
+}
+void vypocitaj_min(int pole[]) {
 
-int sucet(int a, int b) {
-    return a+b;
+}
+void vypocitaj_vacsie_nez_priemer(int pole[], double priemer) {
+
+}
+void vypocitaj_mensie_nez_priemer(int pole[], double priemer) {
+
 }
